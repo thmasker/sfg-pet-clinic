65a2f3a1ec3027a196db90f9939cf2991a27daf5
thmasker <diego.p.hidalgo@gmail.com>
2020-10-30 13:26:56 +0100

    Fix Error

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile("spingdatajpa")
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
   private final PetRepository petRepository;
